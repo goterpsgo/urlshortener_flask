4346b45c8d9e1b1b87eec243632f2d88fdb309e0
Joe Tseng <jtsengorg@gmail.com>
2020-09-13 16:19:20 -0400

    [WIP] Added template for index, includes user login and url forms.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    # return "Hello, World!"
+    return render_template('index.html', my_string="WHEEEEEEEE", my_list=[0,1,2,3,4,5])
+
+@app.route('/bec')
+def bec():
+    return "I ❤️ Bec"
 
